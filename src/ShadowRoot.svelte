e8c58514b93fef8d1d853808ad54ab911bd7f7d6
stefanovualto <stefano.nepa@vualto.com>
2021-02-12 10:28:08 +0000

    imrpove example using typescript files

--- src/ShadowRoot.svelte
+++ src/ShadowRoot.svelte
@@ -1,9 +1,20 @@
 <svelte:options tag="svelte-component-ts" />
 
 <script lang="ts">
+    import Hello from './components/Hello.svelte';
+    import { reverse } from './utils/reverse';
     let message: string = 'Hello world!';
+    $: revMessage = reverse(message);
 </script>
 
-<div>
-    {message}
+<div class="un-scoped-class">
+    {revMessage}
+    <Hello />
 </div>
+
+<style>
+    /* Some styles are needed to be able to inject sub styles currently, probably need to hack more to avoid this requirement 🤷‍♂️*/
+    .un-scoped-class {
+        background-color: inherit;
+    }
+</style>
---
+++ src/components/Hello.svelte
@@ -0,0 +1,15 @@
+<div>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni enim
+        modi commodi iusto aut itaque quibusdam ea ad eveniet atque. Consectetur
+        libero incidunt doloremque aspernatur in, illo quidem magnam minus.
+    </p>
+</div>
+
+<style lang="scss">
+    div {
+        p {
+            background-color: aquamarine;
+        }
+    }
+</style>
