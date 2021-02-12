8f7e7398aa2cfadf5ddc3a1cffbe2bdecc9ad871
stefanovualto <stefano.nepa@vualto.com>
2021-02-12 10:27:53 +0000

    tidy up roolup plugin + add prettierrc config

--- .rollup/css-only.js
+++ .rollup/css-only.js
@@ -1,14 +1,14 @@
-import { createFilter } from "@rollup/pluginutils";
+import { createFilter } from '@rollup/pluginutils';
 
 function css(options) {
     if (options === void 0) options = {};
 
     var filter = createFilter(
-        ["**/*.css", "**/*.svelte", "**/*.js", "**/*.ts"],
+        ['**/*.css', '**/*.svelte', '**/*.js', '**/*.ts'],
         options.exclude
     );
     var isCssOnly = createFilter(
-        options.include || ["**/*.css"],
+        options.include || ['**/*.css'],
         options.exclude
     );
     var styles = {};
@@ -17,75 +17,38 @@ function css(options) {
     var changes = 0;
 
     return {
-        name: "css",
+        name: 'css',
         transform: function transform(code, id) {
             if (!isCssOnly(id)) {
                 return;
             }
 
-            // // When output is disabled, the stylesheet is exported as a string
-            // if (options.output === false) {
-            //     return {
-            //         code: "export default " + JSON.stringify(code),
-            //         map: { mappings: "" },
-            //     };
-            // }
-
-            // // Track the order that each stylesheet is imported.
+            // Track the order that each stylesheet is imported.
             if (!order.includes(id)) {
                 order.push(id);
             }
 
-            // // Keep track of every stylesheet
-            // // Check if it changed since last render
+            // Keep track of every stylesheet
+            // Check if it changed since last render
             if (styles[id] !== code && (styles[id] || code)) {
                 styles[id] = code;
                 changes++;
             }
-            return "";
+            return '';
         },
         generateBundle: function generateBundle(opts, bundle) {
-            // No stylesheet needed
-            // if (!changes || options.output === false) {
-            //     return;
-            // }
-            // changes = 0;
-
             // Combine all stylesheets, respecting import order
-            var css = "";
+            var css = '';
             for (var x = 0; x < order.length; x++) {
                 var id = order[x];
-                css += styles[id] || "";
+                css += styles[id] || '';
             }
 
             // Emit styles through callback
-            if (typeof options.output === "function") {
+            if (typeof options.output === 'function') {
                 options.output(css, styles, bundle);
                 return;
             }
-
-            // if (typeof dest !== "string") {
-            //     // Don't create unwanted empty stylesheets
-            //     if (!css.length) {
-            //         return;
-            //     }
-
-            //     // Guess destination filename
-            //     dest =
-            //         opts.file ||
-            //         (Array.isArray(opts.output)
-            //             ? opts.output[0].file
-            //             : opts.output && opts.output.file) ||
-            //         opts.dest ||
-            //         "bundle.js";
-            //     if (dest.endsWith(".js")) {
-            //         dest = dest.slice(0, -3);
-            //     }
-            //     dest = dest + ".css";
-            // }
-
-            // // Emit styles to file
-            // this.emitFile({ type: "asset", fileName: dest, source: css });
         },
     };
 }
